3479f92eab98646ea07060ffb0215d0715185f64
Nerio <neriogonzalez@gmail.com>
2018-07-15 23:40:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nerio/intent/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.nerio.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Siguiente (View Vista) {
+        Intent siguiente = new Intent(this, SegundoActivity.class);
+        startActivity(siguiente);
+    }
+}
---
+++ app/src/main/java/com/example/nerio/intent/SegundoActivity.java
@@ -0,0 +1,20 @@
+package com.example.nerio.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SegundoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+
+    public void Anterior (View Vista) {
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+    }
+}
